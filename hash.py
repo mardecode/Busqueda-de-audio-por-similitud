0d123452e4effaef9388e4f6d26b3149e002c5b5
margarcuae <margar.tcuae@gmail.com>
2018-11-24 11:01:15 -0500

    solo insertar

---
+++ hash.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+class hash(object):
+     def __init__(self,size):
+        self.size = size
+        self.tabla = [None] * size
+
+        
+     def insertar(self, dato):
+        ubicacion = dato%self.size
+        while(self.tabla[ubicacion] != None):
+            ubicacion += 1
+            if ubicacion == self.size:
+                ubicacion = 0
+        self.tabla[ubicacion] = dato
+
+     def printo(self):
+         print self.tabla
+
+     def buscar(self,dato):
+         print 9
+
+
+numeros = np.random.randint(36000, size=30)
+
+tab = hash(30)
+
+for i in range(30):
+    tab.insertar(numeros[i])
+tab.printo()
