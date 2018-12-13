9e824ec56de14b2c032c88d5f8d2a5d533c9a25f
margarcuae <margar.tcuae@gmail.com>
2018-12-12 19:21:28 -0500

    insertar2

--- hash.py
+++ hash.py
@@ -33,6 +33,30 @@ class hash(object):
             # print("dato colisionado ",dato)
             self.colisiones += 1
 
+     def insertar2(self,llave,dato):
+        print ("say hi")
+        self.datos +=1
+        ubicacion = llave%self.size
+        hubo = False
+        masVuelta = 0
+
+        while(self.tabla[ubicacion]!= None):
+            if(masVuelta ==2): print("El tamanio de la data es mas grande que la tabla \n Presiona Crtl + c")
+            if(self.tabla[ubicacion][0] == llave):
+                self.tabla[ubicacion][2].append(dato)
+                self.tabla[ubicacion][1] += 1
+                break
+            hubo = True
+            ubicacion += 1
+            if ubicacion == self.size:
+                masVuelta +=1
+                ubicacion =0
+
+
+        if(self.tabla[ubicacion] == None):
+            self.tabla[ubicacion] = [llave,1,[]]    
+            self.tabla[ubicacion][2].append(dato)
+            self.ocupados += 1 
 
      def printo(self):
          print (self.tabla)
@@ -65,19 +89,25 @@ class hash(object):
 
 # tab = hash(tamano)
 
-# tab.insertar(1,34)
-# tab.insertar(1,22)
-# tab.insertar(11,11)
-# tab.insertar(2,34)
-# tab.insertar(2,19)
-# tab.insertar(21,44)
-# tab.insertar(22,89)
-# tab.insertar(4,11)
-# tab.insertar(44,34)
-# tab.insertar(5,15)
-# tab.insertar(6,89)
-# tab.insertar(8,21)
-# tab.insertar(8,31)
+# tab.insertar2(1,34)
+# tab.insertar2(1,22)
+# tab.insertar2(1,23)
+# tab.insertar2(1,24)
+# tab.insertar2(1,25)
+# tab.insertar2(1,28)
+# tab.insertar2(1,29)
+
+# tab.insertar2(2,34)
+# tab.insertar2(2,19)
+
+# tab.insertar2(4,11)
+
+# tab.insertar2(5,15)
+# tab.insertar2(5,35)
+
+# tab.insertar2(6,89)
+# tab.insertar2(8,21)
+# tab.insertar2(8,31)
 
 # tab.printo()
 
